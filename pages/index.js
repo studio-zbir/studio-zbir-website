5c2ef17aaed8b14e4ffee84a62e76038c831ec60
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2024-08-18 22:48:11 +0200

    Setup NextJS and CI action

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Studio Zbir</title>
+                <meta name="description"
+                      content="Architecture studio specializing in interior design and turnkey solutions"/>
+            </Head>
+
+            <main>
+                <h1>
+                    Welcome to Studio Zbir
+                </h1>
+                <p>
+                    Architecture studio specializing in interior design and turnkey solutions
+                </p>
+            </main>
+
+            <footer>
+            </footer>
+        </>
+    );
+}
